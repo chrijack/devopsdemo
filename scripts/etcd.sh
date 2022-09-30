ab86328a6698f23a173cf88c57d39cfad245be19
chrijack@cisco.com <chrijack@cisco.com>
2022-09-30 13:06:13 -0400

    update helm chart

--- scripts/etcd.sh
+++ scripts/etcd.sh
@@ -4,7 +4,7 @@ echo "initializing Helm"
 helm init --wait --debug
 
 echo "installing etcd operator (Helm Chart)"
-helm install stable/etcd-operator --version 0.8.0 --name etcd-operator --debug
+helm install stable/etcd-operator --version 0.11.2 --name etcd-operator --debug
 kubectl rollout status deploy/tiller-deploy -n kube-system
 
 kubectl  create -f manifests/etcd-cluster.yaml
